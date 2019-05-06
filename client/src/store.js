a46c3d953c6b24491a855f971020379ce27f506c
Tigran Mkrtchyan <2492460@gmail.com>
2019-05-06 23:15:47 +0300

    Compose issue production fixed

--- client/src/store.js
+++ client/src/store.js
@@ -5,14 +5,19 @@ import rootReducer from "./reducers";
 const initialState = {};
 
 const middleware = [thunk];
+//
+let composeStore;
 
-const store = createStore(
-  rootReducer,
-  initialState,
-  compose(
+//
+if (process.env.NODE_ENV === "production") {
+  composeStore = compose(applyMiddleware(...middleware));
+} else {
+  composeStore = compose(
     applyMiddleware(...middleware),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+  );
+}
+
+const store = createStore(rootReducer, initialState, composeStore);
 
 export default store;
